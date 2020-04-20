e25cb3c626065981fcb299d73bcf3b4c94e21d3e
The_Fireplace <the.f1repl4ce@gmail.com>
2020-04-20 00:13:32 -0500

    Initial commit

---
+++ src/main/java/the_fireplace/grandeconomy/vaultcompat/GrandEconomyVaultCompat.java
@@ -0,0 +1,26 @@
+package the_fireplace.grandeconomy.vaultcompat;
+
+import com.google.common.collect.Lists;
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.ServicePriority;
+import org.bukkit.plugin.java.JavaPlugin;
+import the_fireplace.grandeconomy.GrandEconomy;
+
+public final class GrandEconomyVaultCompat extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        super.onEnable();
+        if(!Lists.newArrayList("bukkit", "vault").contains(GrandEconomy.cfg.economyBridge.toLowerCase()))
+            Bukkit.getServer().getServicesManager().register(Economy.class, new OtherEconHandler(this), this, ServicePriority.High);
+        else
+            //noinspection deprecation
+            GrandEconomy.setEconomy(new VaultEconHandler());
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+        Bukkit.getServer().getServicesManager().unregisterAll(this);
+    }
+}
